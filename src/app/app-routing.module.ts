cd4ee6d66f6d03282b346d7017a23265c32b632d
tawfeek <tawfeeksami@gmail.com>
2019-02-01 15:44:14 +0200

    deploying

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserstableComponent } from './AdminUsersProject/Components/userstable/userstable.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: UserstableComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
